d990e6c06979391aa133f463eba15621c886563e
hfut <hfut@hfut.com>
2026-03-30 17:15:16 +0800

    demo01-b4

--- src/rm_auto_aim/gimbal_pipeline/include/gimbal_pipeline/gimbal_pipeline_node.hpp
+++ src/rm_auto_aim/gimbal_pipeline/include/gimbal_pipeline/gimbal_pipeline_node.hpp
@@ -221,9 +221,6 @@ class GimbalPipelineNode : public rclcpp::Node {
   // Publishers
   rclcpp::Publisher<rm_interfaces::msg::GimbalCmd>::SharedPtr gimbal_cmd_pub_;
 
-  // Heartbeat
-  HeartBeatPublisher::SharedPtr heartbeat_;
-
   // Maneuver states publisher (always-on, for chart monitoring)
   rclcpp::Publisher<rm_interfaces::msg::ManeuverStates>::SharedPtr
       maneuver_states_pub_;
@@ -252,7 +249,10 @@ class GimbalPipelineNode : public rclcpp::Node {
   // TF2
   std::shared_ptr<tf2_ros::Buffer> tf2_buffer_;
   std::shared_ptr<tf2_ros::TransformListener> tf2_listener_;
-  
+
+  // Heartbeat monitor for critical components
+  HeartBeatPublisher::SharedPtr heartbeat_;
+
   /* ================================================================ */
   /*  Prediction logger (optional, controlled by logging.enable)   */
   /* ================================================================ */
